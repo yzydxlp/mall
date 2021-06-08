102a9d6bbe5bf036d65f72ce12e44d932740214b
yzydxlp <1036185354@qq.com>
2021-06-08 21:09:49 +0800

    6_8学习记录

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,15 +1,48 @@
 <template>
-  <div>
-    <h2>首页</h2>
+  <div id="home">
+    <nav-bar class="home-nav"><div slot="center">购物车</div></nav-bar>
+    <home-swiper :banners="banners"/>
+    <home-recommend-view :recommends="recommends"/>
   </div>
 </template>
 
 <script>
-export default {
-  name:"Home"
-}
+  import NavBar from '../../components/common/navbar/NavBar.vue'
+  import HomeSwiper from './childComps/HomeSwiper.vue'
+  import HomeRecommendView from './childComps/HomeRecommendView.vue'
+  import {getHomeMultidata} from "../../network/home"
+  
+  
+
+  export default {
+    name:"Home",
+    components: {
+      NavBar,
+      HomeSwiper,
+      HomeRecommendView,
+    },
+    data() {
+      return {
+        banners: [],
+        recommends: []
+      }
+    },
+    created() {
+      //1.请求多个数据
+      getHomeMultidata().then( res  => {
+        console.log(res);
+        this.banners = res.data.banner.list
+        this.recommends = res.data.recommend.list
+      })
+      //
+      //
+    },
+  }
 </script>
 
 <style>
-
+  .home-nav{
+    background-color: var(--color-tint);
+    color: white;
+  }
 </style>
---
+++ src/views/home/childComps/HomeSwiper.vue
@@ -0,0 +1,32 @@
+<template>
+  <swiper>
+      <swiper-item v-for="item in banners" :key="item">
+        <a :href="item.link">
+          <img :src="item.image" alt="">
+        </a>
+      </swiper-item>
+    </swiper>
+</template>
+
+<script>
+  import {Swiper,SwiperItem} from '../../../components/common/swiper/index.js'
+  export default {
+    name: 'HomeSwiper',
+    props: {
+      banners: {
+        tyoe:Array,
+        default() {
+          return []
+        }
+      }
+    },
+    components: {
+      Swiper,
+      SwiperItem
+    }
+  }
+</script>
+
+<style>
+
+</style>
